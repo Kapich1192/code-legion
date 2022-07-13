d893799443392e13a973bde023bbccdc09428116
kapich1192 <kapich1192@yandex.ru>
2022-07-13 15:56:33 +0300

    developing

---
+++ src/main/java/com/codelegion/Code/Legion/repo/ArticleRepository.java
@@ -0,0 +1,7 @@
+package com.codelegion.Code.Legion.repo;
+
+import com.codelegion.Code.Legion.models.articles.Article;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ArticleRepository extends CrudRepository<Article, Long> {
+}
